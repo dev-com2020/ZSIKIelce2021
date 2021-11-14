1da834876d931017759c1078bc2a35de681ba1b7
DEV-COM <kwp@tlen.pl>
2021-11-14 13:37:08 +0100

    kod z żółwiami cd2

--- pakiet/wyscig.py
+++ pakiet/wyscig.py
@@ -1,20 +1,84 @@
 import turtle
+import turtle as t
+import time
+import random
 
-t = turtle.Turtle()
-t.shape('turtle')
-t.color('red')
-t.pensize(7)
-t.penup()
-t.goto(-200,100)
-t.pendown()
-t.forward(300)
+t.bgcolor('black')
+t.title('Wyścig żółwi!')
 
-t.shape('turtle')
-t.color('blue')
-t.pensize(7)
-t.penup()
-t.goto(-200,-100)
-t.pendown()
-t.forward(300)
+meta = turtle.Turtle()
+meta.color('white')
+meta.penup()
+meta.goto(240, 220)
+meta.write("META", font=('Verdana', 15, "bold"))
+
+meta.penup()
+meta.goto(300, 200)
+meta.pendown()
+meta.goto(300, -200)
+meta.hideturtle()
+
+red = turtle.Turtle()
+red.shape('turtle')
+red.color('red')
+red.pensize(7)
+red.penup()
+red.goto(-200, 100)
+red.pendown()
+
+blue = turtle.Turtle()
+blue.shape('turtle')
+blue.color('blue')
+blue.pensize(7)
+blue.penup()
+blue.goto(-200, -100)
+blue.pendown()
+
+odliczanie = turtle.Turtle()
+odliczanie.color('yellow')
+odliczanie.penup()
+odliczanie.goto(-200,200)
+odliczanie.hideturtle()
+
+for x in range(3):
+    odliczanie.write(3-x,font=('Arial',30,'italic'))
+    time.sleep(1)
+    odliczanie.clear()
+
+odliczanie.write("Start !!!",font=("Arial",40,'bold'))
+
+first = random.choice(['red','blue'])
+
+def sprawdz():
+    if red.position()[0] >= 300:
+        odliczanie.clear()
+        odliczanie.color('red')
+        odliczanie.write("Wygrał czerwony!",font=("Arial",40,'bold'))
+        return True
+
+    if blue.position()[0] >= 300:
+        odliczanie.clear()
+        odliczanie.color('blue')
+        odliczanie.write("Wygrał niebieski!",font=("Arial",40,'bold'))
+        return True
+
+    return False
+
+
+while True:
+
+    if first == 'red':
+        red.forward(random.randint(0,70))
+        if sprawdz():break
+        blue.forward(random.randint(0,70))
+        if sprawdz(): break
+    else:
+        blue.forward(random.randint(0, 70))
+        if sprawdz(): break
+        red.forward(random.randint(0, 70))
+        if sprawdz(): break
+
+    time.sleep(0.3)
+
 
 turtle.exitonclick()
