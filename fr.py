280913f83149cc13117d43447dd54f058ba62f19
DEV-COM <kwp@tlen.pl>
2021-12-12 11:15:47 +0100

    rozpoznawanie twarzy cz2

--- fr.py
+++ fr.py
@@ -9,3 +9,13 @@ face_locations = face_recognition.face_locations(image)
 number_of_faces = len(face_locations)
 
 print("Znalazłem {} twarzy na fotografii".format(number_of_faces))
+
+pil_image = PIL.Image.fromarray(image)
+
+for face_location in face_locations:
+    top, right, bottom, left = face_location
+    print("Twarz znajduje się w lokalizacji pikseli: \n"
+          "Top {}, Left {}, Bottom {}, Right {}".format(top, left, bottom, right))
+    draw = PIL.ImageDraw.Draw(pil_image)
+    draw.rectangle([left, top, right, bottom], outline='red')
+pil_image.show()
